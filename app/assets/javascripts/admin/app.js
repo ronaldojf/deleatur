60a37cdc81b3a4a1a5899bf36270de4c2cbc195c
Ronaldo Fuzinato <ronaldofuzinato123@gmail.com>
2015-08-06 12:52:52 -0300

    Alterado escopo do BaseController do angular para um escopo geral

--- app/assets/javascripts/admin/app.js
+++ app/assets/javascripts/admin/app.js
@@ -1,6 +1,5 @@
 //= require ../application
 //= require inspinia/inspinia
-//= require ./angular/controllers/BaseController
 //= require_self
 //= require_tree ./angular/controllers
 
--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,5 +1,6 @@
 //= require angular/custom-filters
 //= require angular/deleatur
+//= require angular/controllers/BaseController
 
 $(document).on('page:change', function(){
   if (!$('body').scope()) {
