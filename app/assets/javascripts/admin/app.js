7dabdc9d6753b41bc258c81ef97072ae6f632088
Ronaldo Fuzinato <ronaldofuzinato123@gmail.com>
2015-08-29 15:26:36 -0300

    Fix: build failing

--- app/assets/javascripts/admin/app.js
+++ app/assets/javascripts/admin/app.js
@@ -1,6 +1,6 @@
 //= require ../application
 //= require inspinia/inspinia
 //= require_self
-//= require_tree ./angular/controllers
+// require_tree ./angular/controllers
 
 window.baseController = deleatur._invokeQueue[3][2][1][4];
